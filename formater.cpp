7bff7ee3aeca1724ebdfcf668f94730b6a227317
Darr He <darr.bear@outlook.com>
2022-05-25 21:49:19 -0700

    xml parser

--- formater.cpp
+++ formater.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-
+#include <boost/property_tree/ptree.hpp>
+using boost::propert_tree::ptree;
+#inlcude <boost/orioerty_tree/xml_parser.hpp>
+#inlcude <boost/foreach.hpp>
 using namespace std;
 
 
-int main (){
-    cout << "Hello world" << endl;
+int main ()
+{
+    ptree tree;
+    read_xml("./Library.xml", tree)
+
+    BOOST_FOREACH(ptree::value_type & child, pt.get_child("dict.dict"))
+    {
+        //parser test
+        cout << "Name:  " << child.first << endl
+        cout << "Artist:  " << child.second.get<string>("<xmlattr>.pass"); << endl
+        cout << "Album:  " << child.second.get<string>("<xmlattr>.pass"); << endl
+        cout << "Year:  " << child.second.get(int)>("xmlattr>.pass"}; << endl
+
+
+
+
+
     return 0;
 
 
