d0a24d724e89d1cb2cafc45b4598205094305435
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2023-12-15 08:19:28 -0800

    xml string extraction

--- formater.cpp
+++ formater.cpp
@@ -1,30 +1,46 @@
 #include <iostream>
-#include <boost/property_tree/ptree.hpp>
-using boost::propert_tree::ptree;
-#inlcude <boost/orioerty_tree/xml_parser.hpp>
-#inlcude <boost/foreach.hpp>
-using namespace std;
+#include <vector>
+#include "tinyxml2.h"
 
+using namespace tinyxml2;
 
-int main ()
-{
-    ptree tree;
-    read_xml("./Library.xml", tree)
+int main() {
+    XMLDocument doc;
+    std::vector<std::string> songInfo; // Array to store Name, Artist, and Album
 
-    BOOST_FOREACH(ptree::value_type & child, pt.get_child("dict.dict"))
-    {
-        //parser test
-        cout << "Name:  " << child.second.get<string>("<xmlattr>.Name") << endl
-        cout << "Artist:  " << child.second.get<string>("<xmlattr>.Artist"); << endl
-        cout << "Album:  " << child.second.get<string>("<xmlattr>.Album"); << endl
-        cout << "Year:  " << child.second.get(int)>("xmlattr>.Year"}; << endl
+    // Load the XML file
+    if (doc.LoadFile("Music.xml") == XML_SUCCESS) {
+        // Get the root element
+        XMLElement* root = doc.RootElement();
+
+        // Iterate through each 'dict' element
+        for (XMLElement* dict = root->FirstChildElement("dict"); dict; dict = dict->NextSiblingElement("dict")) {
+            // Get the 'Name', 'Artist', and 'Album' elements
+            XMLElement* nameElement = dict->FirstChildElement("key")->NextSiblingElement("string");
+            XMLElement* artistElement = nameElement->NextSiblingElement("key")->NextSiblingElement("string");
+            XMLElement* albumElement = artistElement->NextSiblingElement("key")->NextSiblingElement("string");
+
+            // Check if the elements are not null
+            if (nameElement && artistElement && albumElement) {
+                // Get the text content of 'Name', 'Artist', and 'Album'
+                std::string name = nameElement->GetText();
+                std::string artist = artistElement->GetText();
+                std::string album = albumElement->GetText();
+
+                // Store the information in the array
+                songInfo.push_back(name);
+                songInfo.push_back(artist);
+                songInfo.push_back(album);
+            }
+        }
+
+        // Print the stored information
+        for (const auto& info : songInfo) {
+            std::cout << info << std::endl;
+        }
+    } else {
+        std::cerr << "Error loading XML file." << std::endl;
+    }
 
     return 0;
-
-
 }
-    cout << "Hello world!" << endl;
-    return 0;
-
-
-}P
